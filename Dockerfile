9debebbf3dca2768ba8f70e501629799fc6c671d
kenf1 <103538771+kenf1@users.noreply.github.com>
2024-10-26 12:38:08 +0000

    Create mpa

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:current-slim
 
 RUN npm install -g typescript \
     vite create-vite \
-    react react-dom \
+    react react-dom react-router-dom \
 	npkill
 RUN apt-get update && apt-get install -y git \
     make
--- webterm/src/App.tsx
+++ webterm/src/App.tsx
@@ -1,70 +1,20 @@
-import React, { useState } from "react"
-import "./App.css"
-import { prepFunc, sumNumArray, inputX2 } from "../../luhn-algo/func.ts"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./Static/App.css"
+
+import { Term } from "./Routes/Term.tsx"
+import { NotFound } from "./Routes/Fallback.tsx"
+import { LuhnDebug } from "./Routes/LuhnDebug.tsx"
 
 function App() {
-  let [userInput, setUserInput] = useState("");
-  let [singleSum, setSingleSum] = useState<number>(0);
-  let [resultMessage, setResultMessage] = useState("");
-
-  const luhnAlgo = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //get user input & store as var
-    let inputVal: string = event.target.value;
-    setUserInput(inputVal);
-
-    /*
-      rm all spaces + non-numbers
-      double every other digit (exclude last digit in rev-mode)
-      sum digits of doubled number if >= 10
-    */
-    let processedArray: number[] = inputX2(
-      prepFunc(inputVal)
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Term />} />
+                <Route path="/luhn-debug" element={<LuhnDebug />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </Router>
     );
-
-    //sum all digits
-    let sum: number = sumNumArray(processedArray);
-    if (isNaN(sum)) {
-      setSingleSum(-1);
-    } else {
-      setSingleSum(sum);
-    }
-
-    //outcome
-    let remainder: number = sum % 10;
-    if (remainder === 0) {
-      setResultMessage('PASS');
-    } else {
-      setResultMessage('FAIL');
-    }
-  }
-
-  return (
-    <>
-      <div>
-        <p className="res-desc">
-          Output:
-        </p>
-
-        <p className="res-actual">
-          Input returned a total sum: {singleSum === -1 ? '-' : singleSum}
-        </p>
-
-        <p className="res-actual">
-          <span className={resultMessage === 'PASS' ? 'pass' : 'fail'}>{resultMessage}</span>
-        </p>
-
-      </div>
-
-      <div className="input-box">
-        <input
-          type="text"
-          value={userInput}
-          onChange={luhnAlgo}
-          placeholder="$"
-        />
-      </div>
-    </>
-  )
 }
 
 export default App
