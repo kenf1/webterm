7cefe0cbc40b084c01d1f48956469f7673d61a9a
kenf1 <103538771+kenf1@users.noreply.github.com>
2024-10-26 00:55:41 +0000

    Add css

--- webterm/src/App.tsx
+++ webterm/src/App.tsx
@@ -1,36 +1,70 @@
 import React, { useState } from "react"
 import "./App.css"
+import { prepFunc, sumNumArray, inputX2 } from "../../luhn-algo/func.ts"
 
 function App() {
   const [userInput, setUserInput] = useState("");
+  const [singleSum, setSingleSum] = useState<number>(0);
+  const [resultMessage, setResultMessage] = useState("");
 
-  const handleUserInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const luhnAlgo = (event: React.ChangeEvent<HTMLInputElement>) => {
     //get user input & store as var
     const inputVal: string = event.target.value;
     setUserInput(inputVal);
+
+    /*
+      rm all spaces + non-numbers
+      double every other digit (exclude last digit in rev-mode)
+      sum digits of doubled number if >= 10
+    */
+    const processedArray: number[] = inputX2(
+      prepFunc(inputVal)
+    );
+
+    //sum all digits
+    const sum: number = sumNumArray(processedArray);
+    if (isNaN(sum)) {
+      setSingleSum(-1);
+    } else {
+      setSingleSum(sum);
+    }
+
+    //outcome
+    const remainder: number = sum % 10;
+    if (remainder === 0) {
+      setResultMessage('PASS');
+    } else {
+      setResultMessage('FAIL');
+    }
   }
 
   return (
     <>
+      <div>
+        <p className="res-desc">
+          Output:
+        </p>
+
+        <p className="res-actual">
+          The input returned a total sum of {singleSum === -1 ? '-' : singleSum}
+        </p>
+
+        <p className="res-actual">
+          <span className={resultMessage === 'PASS' ? 'pass' : 'fail'}>{resultMessage}</span>
+        </p>
+
+      </div>
+
       <div className="input-box">
+        {/* <p className="term-user">
+          user:
+        </p> */}
         <input
           type="text"
           value={userInput}
-          onChange={handleUserInput}
-          placeholder="Enter value:"
+          onChange={luhnAlgo}
+          placeholder="$"
         />
-      </div>
-
-      <div>
-        <p className="res-desc">
-          You entered:
-        </p>
-
-        <br />
-
-        <p className="res-actual">
-          {userInput}
-        </p>
       </div>
     </>
   )
