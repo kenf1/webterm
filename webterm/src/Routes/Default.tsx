cd3aafda55a33e9889bfe8749efb820653248fd6
kenf1 <103538771+kenf1@users.noreply.github.com>
2024-10-27 15:34:32 +0000

    Terminal theme

--- webterm/src/Routes/Default.tsx
+++ webterm/src/Routes/Default.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { argIndex0, cmdHandler } from "../../Logic/parseArgs";
-import "../Static/Default.css";
+import "../Static/gif.css";
 
 export function DefRoute() {
     let [userInput, setUserInput] = useState("");
